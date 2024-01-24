05a81a3d7b0518db84b75bb9e16d7cdf7230d4bf
Marat Shagiev <21020173+dumbm1@users.noreply.github.com>
2024-01-24 18:50:17 +0300

    Added iconSprite.svg, some changed componets, some changed themisation

--- assets/js/themeChanger.js
+++ assets/js/themeChanger.js
@@ -12,31 +12,45 @@ function changeTheme(csInterface) {
   let fontSize = appSkinInfo.baseFontSize;
   let appBgColor = appSkinInfo.panelBackgroundColor.color;
   let html = document.documentElement;
+
   let fontFam = appSkinInfo.baseFontFamily;
 
   console.log(appBgColor);
 
-  document.body.style.fontSize = fontSize + 'px';
+  html.style.fontSize = fontSize + 'px';
+  // document.body.style.fontSize = fontSize + 'px';
 
-  if (appBgColor.red < 180) {
+/*  if (appBgColor.red < 180) {
    html.setAttribute('data-theme-dark', '');
    console.log("Dark theme");
   } else {
    html.removeAttribute('data-theme-dark');
    console.log("Light theme");
-  }
+  }*/
   if (appBgColor.red == 50) {
-   document.body.style.background = '#323232';
-   console.log('bg darker');
+ /*  document.body.style.background = '#323232';
+   console.log('bg darker');*/
+   html.removeAttribute('data-theme-dark');
+   html.removeAttribute('data-theme-light');
+   html.setAttribute('data-theme-darker', '');
   } else if (appBgColor.red == 83) {
-   document.body.style.background = '#535353';
-   console.log('gb dark');
+/*   document.body.style.background = '#535353';
+   console.log('gb dark');*/
+   html.removeAttribute('data-theme-darker');
+   html.removeAttribute('data-theme-light');
+   html.setAttribute('data-theme-dark', '');
   } else if (appBgColor.red == 184) {
-   document.body.style.background = '#b8b8b8';
-   console.log('bg light');
+   html.removeAttribute('data-theme-dark');
+   html.removeAttribute('data-theme-darker');
+   html.setAttribute('data-theme-light', '');
+  /* document.body.style.background = '#b8b8b8';
+   console.log('bg light');*/
   } else if (appBgColor.red == 240) {
-   document.body.style.background = '#f0f0f0';
-   console.log('bg lighter');
+   html.removeAttribute('data-theme-dark');
+   html.removeAttribute('data-theme-darker');
+   html.removeAttribute('data-theme-light');
+  /* document.body.style.background = '#f0f0f0';
+   console.log('bg lighter');*/
   }
  }
 
