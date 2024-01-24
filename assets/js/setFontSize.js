0d5b5ae62583f2f3d5d622de4eea7dacb5cff03f
Marat Shagiev <21020173+dumbm1@users.noreply.github.com>
2024-01-24 23:17:38 +0300

    Finished the theming (to some extent). Next: making some interface components.

--- assets/js/setFontSize.js
+++
@@ -1,6 +0,0 @@
-/*
-let csInterface = new CSInterface();
-let appSkinInfo = csInterface.hostEnvironment.appSkinInfo;
-let fontSize = appSkinInfo.baseFontSize;
-document.getElementsByTagName('body').style.fontSize = fontSize + 'px';
-alert('hello');*/
--- assets/js/themeChanger.js
+++ assets/js/themeChanger.js
@@ -18,39 +18,15 @@ function changeTheme(csInterface) {
   console.log(appBgColor);
 
   html.style.fontSize = fontSize + 'px';
-  // document.body.style.fontSize = fontSize + 'px';
 
-/*  if (appBgColor.red < 180) {
-   html.setAttribute('data-theme-dark', '');
-   console.log("Dark theme");
-  } else {
-   html.removeAttribute('data-theme-dark');
-   console.log("Light theme");
-  }*/
   if (appBgColor.red == 50) {
- /*  document.body.style.background = '#323232';
-   console.log('bg darker');*/
-   html.removeAttribute('data-theme-dark');
-   html.removeAttribute('data-theme-light');
-   html.setAttribute('data-theme-darker', '');
+   html.setAttribute('data-theme', 'darker');
   } else if (appBgColor.red == 83) {
-/*   document.body.style.background = '#535353';
-   console.log('gb dark');*/
-   html.removeAttribute('data-theme-darker');
-   html.removeAttribute('data-theme-light');
-   html.setAttribute('data-theme-dark', '');
+   html.setAttribute('data-theme', 'dark');
   } else if (appBgColor.red == 184) {
-   html.removeAttribute('data-theme-dark');
-   html.removeAttribute('data-theme-darker');
-   html.setAttribute('data-theme-light', '');
-  /* document.body.style.background = '#b8b8b8';
-   console.log('bg light');*/
+   html.setAttribute('data-theme', 'light');
   } else if (appBgColor.red == 240) {
-   html.removeAttribute('data-theme-dark');
-   html.removeAttribute('data-theme-darker');
-   html.removeAttribute('data-theme-light');
-  /* document.body.style.background = '#f0f0f0';
-   console.log('bg lighter');*/
+   html.removeAttribute('data-theme');
   }
  }
 
